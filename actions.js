9d5bcb1801025056dc7965e322d11e870cc3043d
Bima Saridjo <palessie27@gmail.com>
2020-07-08 18:57:53 -0400

    push stretch

--- actions.js
+++ actions.js
@@ -4,4 +4,30 @@ const axios = require('axios');
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
 /* YOUR CODE GOES HERE */
+const gotDesserts = (arr) => {
+    return {
+        type: GOT_DESSERTS_DATA,
+        desserts: arr
+    }
+}
+
+const addDessert = (arr) => {
+    return {
+        type: NEW_DESSERT_DATA,
+        newDessert: arr
+    }
+}
+
+const fetchDesserts = async (arr) => {
+    return new Promise(res => {
+        if(res){
+            return gotDesserts(arr);
+        }
+    })
+}
+
+const postDessert = (arr) => {
+}
+
+
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
